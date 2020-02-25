f5c811db4c538ddccd0f7c7f079a74deb500a91c
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2020-02-25 23:42:07 +0530

    Program on how to initialize a vector

---
+++ Vector.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	vector <char> vowels{ 'a','e','i','o','u' };
+	vector <int> test_scores{ 10,20,30,40 };
+	vector <double> days_in_year(365, 90);
+
+
+}
+
+
