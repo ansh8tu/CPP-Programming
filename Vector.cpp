1ecc972479893e4e3e5029720aba76f882c74345
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2020-02-26 00:58:50 +0530

    use of .at(), .size() and .push_back() commands

--- Vector.cpp
+++ Vector.cpp
@@ -7,6 +7,19 @@ int main()
 	vector <char> vowels{ 'a','e','i','o','u' };
 	vector <int> test_scores{ 10,20,30,40 };
 	vector <double> days_in_year(365, 90);
+	
+	//array syntax
+	cout << days_in_year[9] << endl;
+
+	//vector syntax
+	cout << test_scores.at(0) << endl;
+
+	//use of push_back command
+	test_scores.push_back(80);
+	cout << test_scores[4] << endl;
+
+	//use of size() command
+	cout << "The size of test_score vector is: " << test_scores.size()
 
 
 }
